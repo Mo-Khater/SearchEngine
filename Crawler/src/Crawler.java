31e1a691a99e8c1478e24eda1481c440dbf69862
MHW <118685507+Monwilliam@users.noreply.github.com>
2024-04-06 23:11:44 +0200

    threads

--- Crawler/src/Crawler.java
+++ Crawler/src/Crawler.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -8,44 +10,70 @@ import org.jsoup.nodes.Element;
 import javax.print.Doc;
 
 
-public class Crawler {
+public class Crawler implements Runnable{
 
-    public static void main(String[] args) {
-        String url= "https://www.wikipedia.org/";
-        crawl(1,url,new ArrayList<String>());
+    private final int maxsize;
+    private ArrayList<String> mainvisited;
+    private ArrayList<String> suppvisited=new ArrayList<String>();
+    private Thread thread;
+    private Queue<String> queue;
+    private final int ID;
+    private final Object o;
+
+
+    public Crawler(int id,ArrayList<String>s,ArrayList<String>v,int max_s,Object to_sync)
+    {
+        ID=id;
+        o=to_sync;
+        mainvisited=v;
+        maxsize=max_s;
+        queue=new LinkedList<>();
+        for(int e=0;e<s.size();e++)
+            queue.offer(s.get(e));
+        thread=new Thread(this);
     }
 
-    private static void crawl(int level, String url,ArrayList<String>visited)
+    public void start_thread()
     {
-        if(level <= 5)
+     thread.start();
+    }
+
+    @Override
+    public void run() {
+        while(!queue.isEmpty() && suppvisited.size() != maxsize)
         {
-            Document doc=request(url,visited);
-            if(doc != null)
-            {
-                for(Element link : doc.select("a[href]"))
-                {
-                    String nextlink = link.absUrl("href");
-                    if(!visited.contains(nextlink))
-                        crawl(level++,nextlink,visited);
-                }
+            String url = queue.poll();
+            crawl(url);
+        }
+    }
+
+    private void crawl(String url)
+    {
+        Document doc=request(url);
+        if(doc != null) {
+            for (Element link : doc.select("a[href]")) {
+                String nextlink = link.absUrl("href");
+                if (!queue.contains(nextlink) && !suppvisited.contains(nextlink))
+                    queue.offer(nextlink);
             }
         }
     }
 
-    private static Document request(String url, ArrayList<String> v)
+    private Document request(String url)
     {
         try{
             Connection con = Jsoup.connect(url);
-            Document doc=con.get();
+            Document doc = con.get();
             if(con.response().statusCode() == 200)
             {
-                System.out.println("Link: " + url);
-                System.out.println(doc.title());
-                v.add(url);
+                if(add_url(url))
+                {
+//                System.out.println("thread "+ ID + ": added Link: " + url);
+//                System.out.println(doc.title());
                 return doc;
+                }
             }
             return null;
-
         }
         catch (IOException e)
         {
@@ -53,5 +81,22 @@ public class Crawler {
         }
     }
 
+    private boolean add_url(String url)
+    {
+        synchronized (o)
+        {
+            if(!mainvisited.contains(url)) {
+                mainvisited.add(url);
+                suppvisited.add(url);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public Thread getThread()
+    {
+        return thread;
+    }
 
 }
