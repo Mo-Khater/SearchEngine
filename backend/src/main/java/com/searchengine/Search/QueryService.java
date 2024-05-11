2ce8c01a2686415bd4e450b96d6942f626606bbe
ali-bahr <alibahr313@gmail.com>
2024-05-11 20:39:46 +0300

    edited some logic

--- backend/src/main/java/com/searchengine/Search/QueryService.java
+++ backend/src/main/java/com/searchengine/Search/QueryService.java
@@ -21,6 +21,8 @@ private IndexerRepository indexerRepository;
 @Autowired
 private IndexerDocumentRepository indexerDocumentRepository;
 
+
+// this func to get the indexed word and it is urls from the DB
 public ArrayList<documnentOfWord> getWordDocuments(String w)
 {
     System.out.println(w);
@@ -40,8 +42,10 @@ public ArrayList<documnentOfWord> getWordDocuments(String w)
     return wordDocument;
 }
 
-    public  ArrayList<String> getdocelements(String docURl)
-    {
+
+// this function to get the elements of the document using the URL
+public  ArrayList<String> getdocelements(String docURl)
+{
         @SuppressWarnings("unchecked")
         List<docelement>temp=indexerDocumentRepository.findByUrl(docURl) ;
         ArrayList<String> elements = new ArrayList<>() ;
@@ -49,7 +53,9 @@ public ArrayList<documnentOfWord> getWordDocuments(String w)
             elements.addAll(d.getElements());
         }
         return elements;
-    }
+}
+
+// this function to get the stemming of a word
     public ArrayList<String> stemandremovestopwordsfunc(String text)
     {
 
@@ -75,13 +81,15 @@ public ArrayList<documnentOfWord> getWordDocuments(String w)
     }
     public ArrayList<ArrayList<String>> searchly(String query){
 
+        // result will store the stemming of the query
         ArrayList<String>result = stemandremovestopwordsfunc(query);
-     /*  for(String str:result){
-            System.out.println(str);
-      }
-      */
+
+        // this map is used to accumulate the final score of each URL
         HashMap<String,Double> mp =new HashMap<String,Double>();
+        // ret will store the urls
         ArrayList<String>ret=new ArrayList<String>();
+
+        // calc the final score of each url and update the mp
         for(String str:result){
             ArrayList<documnentOfWord>temp  = getWordDocuments(str) ;
 
@@ -95,28 +103,34 @@ public ArrayList<documnentOfWord> getWordDocuments(String w)
                 }
             }
         }
-        /////////////////////////////this part to handle pharse searching////
+        /****** this part to handle pharse searching (searching by the exact query) ****/
         if(query.contains("\"")){
             // will handle if thier is multiple " in one query we will search for all of them
             String [] arr=query.split("\"");
+            // test will store the pharses that we need to search for
             ArrayList<String>test=new ArrayList<String>();
             for (int i=0;i<arr.length;i++) {
                 if (i % 2 == 1) {
                     test.add(arr[i]);
                 }
             }
+            // will filter the ret array so that only the docs with the excat phrases will be returned
             ArrayList<ArrayList<String>>phraseResult=new ArrayList<ArrayList<String>>();
             for(String doc:ret ){
                 ArrayList<String >toadd =new ArrayList<String>();
                 toadd.add(doc);
-
+                // search for the snippets
                 ArrayList<String>elementsArr=getdocelements(doc);
+                // this will contain the frequences of finding a phrase in the different elements of the same doc so decideing which one to return as snippest
                 ArrayList<Integer>eleFreq = new ArrayList<>() ;
+                // cnt will store how many phrases of test are found in the doc
                 int cnt = 0 ;
-                int i = 0;
-                int mxfrq = 0 ;
-                int mxind = 0 ;
 
+                // variables to store the ind of the element that store the max freq so we can find it in  O(1)
+                int mxfreq = -1 ;
+                int mxind = -1 ;
+
+                int i = 0;
                 for (i=0;i<test.size();i++){
                     boolean found = false ;
                     int ind = 0 ;
@@ -129,6 +143,14 @@ public ArrayList<documnentOfWord> getWordDocuments(String w)
                             elementsArr.set(k,elementsArr.get(k).replace(test.get(i),"<mark>"+test.get(i)+"</mark>" )) ;
                             cntt++;
                         }
+                        // update max element freq
+                        if(mxfreq==-1){
+                            mxfreq = eleFreq.get(k);
+                            mxind = k ;
+                        }else if (eleFreq.get(k)>eleFreq.get(mxind)){
+                            mxfreq = eleFreq.get(k);
+                            mxind = k ;
+                        }
                         ind++;
                     }
                     if(cntt>0)cnt++;
@@ -136,16 +158,18 @@ public ArrayList<documnentOfWord> getWordDocuments(String w)
                         break;
                     }
                 }
+                // if we found all the phrases add the doc to phrase result
                 if(cnt==test.size()){
-                    int maxValue = Collections.max(eleFreq);
-                    int maxIndex = eleFreq.indexOf(maxValue);
-                    toadd.add(elementsArr.get(maxIndex));
+                //    int maxValue = Collections.max(eleFreq);
+                //    int maxIndex = eleFreq.indexOf(maxValue);
+                    toadd.add(elementsArr.get(mxind));
                     phraseResult.add(toadd);
                 }else{
                     mp.remove(doc);
                 }
             }
 
+            //sort the results according to the value of final score stored in mp
             Collections.sort(phraseResult, new Comparator<ArrayList<String>>() {
                 @Override
                 public int compare(ArrayList<String> arr1,ArrayList<String> arr2) {
@@ -157,9 +181,9 @@ public ArrayList<documnentOfWord> getWordDocuments(String w)
 
         }
 
-
+        // retu will store the url and the snnipest both will be stored in an array and the array will be stored in retu
         ArrayList<ArrayList<String>>retu=new ArrayList<ArrayList<String>>();
-
+        // we will iterate of the each word in the doc and if it and the query have the same stemming so we will return that element as snippest
         for(String url:ret ){
             ArrayList<String>toAdd=new ArrayList<>();
             toAdd.add(url);
@@ -200,6 +224,7 @@ public ArrayList<documnentOfWord> getWordDocuments(String w)
             retu.add(toAdd);
         }
 
+        //sort the results according to the value of final score stored in mp
         Collections.sort(retu, new Comparator<ArrayList<String>>() {
             @Override
             public int compare(ArrayList<String> arr1,ArrayList<String> arr2) {
