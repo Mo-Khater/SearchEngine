df683ae4db5b32a0cd049d1d5ba152d1f4d35d51
MHW <118685507+Monwilliam@users.noreply.github.com>
2024-05-14 03:36:00 +0300

    front

--- FrontEnd/src/HomePage/HomePage.js
+++ FrontEnd/src/HomePage/HomePage.js
@@ -13,15 +13,91 @@ export default function HomePage({ seturls, settime }) {
 
     const [query, setQuery] = useState();
 
+    const [ispending, setispending] = useState(false);
+
+    // const[Suggestions, setSuggestions] = useState([
+    //   ["iam good at play", 10],
+    //   ["messi at barcelona", 20],
+    //   ["play football", 15],
+    //   ["play basketball", 18],
+    //   ["coding is fun", 12]
+    // ]);
+
+    const[Suggestions, setSuggestions] = useState([]);
+
+    const [suggestiontoshow, setsuggestiontoshow] = useState();
+
+    const [showSuggestions, setShowSuggestions] = useState(false);
 
     const handleChange = (event) => {
       if (event.key === 'Enter') {
         setQuery(event.target.value);
+        addStringToLocalStorage(event.target.value);
+        //navigate('/urlpage');
       }
     };
 
+    const handleInputChange = (event) => {
+      const newQuery = event.target.value;
+      getSuggestions(newQuery);
+      setShowSuggestions(true);
+    };
+
+    const handleSuggestionClick = (suggestion) => {
+      setQuery(suggestion);
+    };
+
+    const getSuggestions = (newQuery) => {
+      const filteredSuggestions = Suggestions
+      .filter(([suggestion, score]) => {
+        const queryWords = newQuery.toLowerCase().split(' ');
+        return queryWords.every(word => suggestion.toLowerCase().includes(word));
+      })
+      .map(([suggestion, score]) => suggestion);
+
+      setsuggestiontoshow(filteredSuggestions);
+      setShowSuggestions(true);
+    };
+
+    function addStringToLocalStorage(str) {
+      let strings = [];
+    
+      // Get existing strings from localStorage
+      const existingStrings = localStorage.getItem('myStrings');
+      if (existingStrings) {
+        strings = JSON.parse(existingStrings);
+      }
+    
+      // Check if the string is already present
+      if (!strings.includes(str)) {
+        // Add the new string to the array
+        strings.push(str);
+    
+      // Save the updated array back to localStorage
+        localStorage.setItem('myStrings', JSON.stringify(strings));
+      }
+    };
+    
+    // Function to get all strings from localStorage as an array
+    function getAllStringsFromLocalStorage() {
+      const existingStrings = localStorage.getItem('myStrings');
+      return existingStrings ? JSON.parse(existingStrings) : [];
+    };
+
+    useEffect( () =>{
+      const allStrings = getAllStringsFromLocalStorage();
+      setSuggestions(allStrings);
+    },[]);
 
     useEffect(() => {
+    if(query){
+      // setispending(true);
+      // setTimeout(() => {
+      //   setispending(false);
+      //   navigate('/urlpage');
+      // }, 2000);
+
+      setispending(true);
       const startTime = performance.now(); // Start measuring time
 
       axiosInstance.get('/search', {
@@ -29,19 +105,26 @@ export default function HomePage({ seturls, settime }) {
           keyword: query
         }
       }).then(response => {
+        seturls(response.data);
+        setispending(false);
         const endTime = performance.now(); // Stop measuring time
         const elapsedTime = endTime - startTime; // Calculate elapsed time in milliseconds
         console.log('Time taken:', elapsedTime, 'ms');
         settime(elapsedTime/1000);
         console.log(query);
-        seturls(response.data);
         console.log(response.data);
         navigate('/urlpage');
       }).catch(error => {
         console.error('Error searching', error);
       });
+     }
     }, [query]) ;
 
+  
+    // Function to handle input blur
+    const handleInputBlur = () => {
+      setShowSuggestions(false);
+    };
 
   return (
     <div className='homepage'>
@@ -53,9 +136,21 @@ export default function HomePage({ seturls, settime }) {
         <input
         className='searchbar'
         type="text"
+        onBlur={handleInputBlur}
         placeholder="Type your url"
         onKeyPress={handleChange}
+        onChange={handleInputChange}
       />
+      {showSuggestions && (
+        <ul>
+          {suggestiontoshow.map((suggestion, index) => (
+            <li key={index} onClick={() => handleSuggestionClick(suggestion)} >
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
+      {ispending && <div className="loading"></div>}
     </div>
   )
 }
--- FrontEnd/src/UrlPage/Urlpage.js
+++ FrontEnd/src/UrlPage/Urlpage.js
@@ -7,11 +7,59 @@ import axiosInstance from '../apiClient';
 
 export default function Urlpage({ urls,time }) {
 
-  const [newurls,setnewurls] = useState(urls);
+  // const test =[
+  //   ["asdsad20" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad21" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad22" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad23" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad24" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad25" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad26" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad27" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad28" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad29" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad30" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad31" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad32" , "sadasdasdweqewtrgdfgfd"]
+  // ];
 
+  // const [newurls,setnewurls] = useState([
+  //   ["asdsad1" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad2" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad3" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad4" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad5" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad6" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad7" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad8" , "sadasdasdweqewtrgdfgfd"],
+  //   ["asdsad9" , "sadasdasdweqewtrgdfgfd"]
+  // ]);
+
+  const [newurls,setnewurls] = useState(urls);
 
   const [query, setQuery] = useState();
   const [timetaken, settimetaken] = useState(time);
+
+
+
+  const [startIndex, setStartIndex] = useState(0);
+  const [nextavailable, setnextavailable] = useState();
+  const [prevavailable, setprevavailable] = useState();
+  const [ispending ,setispending] = useState(false);
+
+  const showNextArrays = () => {
+    setStartIndex(prevIndex => prevIndex + 10);
+    setprevavailable(true);
+    if(newurls.length - (startIndex) >= 20 ) setnextavailable(true);
+    else{setnextavailable(false);}
+  };
+
+  const showprevArrays = () => {
+    setStartIndex(prevIndex => prevIndex - 10);
+    if(startIndex === 10) setprevavailable(false);
+    setnextavailable(true);
+  };
+
 
   const handleChange = (event) => {
     if (event.key === 'Enter') {
@@ -20,6 +68,15 @@ export default function Urlpage({ urls,time }) {
   };
 
   useEffect(() => {
+    if(query){
+      // setispending(true);
+      // setTimeout(() => {
+      //   setnewurls(test);
+      //   setispending(false);
+      //  }, 2000);
+
+
+    setispending(true);
     const startTime = performance.now(); // Start measuring time
   
     axiosInstance.get('/search', {
@@ -27,17 +84,22 @@ export default function Urlpage({ urls,time }) {
         keyword: query
       }
     }).then(response => {
+      setnewurls(response.data);
+      setispending(false);
       const endTime = performance.now(); // Stop measuring time
       const elapsedTime = endTime - startTime; // Calculate elapsed time in milliseconds
       console.log('Time taken:', elapsedTime, 'ms');
       settimetaken(elapsedTime/1000);
-  
-      setnewurls(response.data);
     }).catch(error => {
       console.error('Error searching', error);
     });
-  }, [query]);
+    }
+  },[query]);
 
+  useEffect(() => {
+    setprevavailable(false);
+    setnextavailable((newurls.length > 10) ? true:false);
+  },[newurls]);
 
   return (
     <div className='urlpage_main'>
@@ -49,7 +111,7 @@ export default function Urlpage({ urls,time }) {
           onKeyPress={handleChange}
         />
         <div className='urls'>
-        {newurls.map((card) => (
+        {newurls.slice(startIndex, startIndex + Math.min(10,newurls.length - startIndex)).map((card) => (
           <div className='element'>
             <a className='url' href={card[0]}>{card[0]}</a>
             <div className='describtion'>
@@ -62,6 +124,9 @@ export default function Urlpage({ urls,time }) {
       <div className='timetaken'>
         <h3>Timetaken : {timetaken}</h3>
       </div>
+      {nextavailable && <button className="next10" onClick={showNextArrays}></button>}
+      {prevavailable && <button className="prev10" onClick={showprevArrays}></button>}
+      {ispending && <div className="loading"></div>}
     </div>
   )
 }
